f6238f99484a07abd58bf0d8f081d066094c7d64
mir2lsky <mir2lsky@gmail.com>
2021-02-01 12:25:09 +0900

    add spring security

---
+++ src/main/java/com/taskagile/config/SecurityConfiguration.java
@@ -0,0 +1,46 @@
+package com.taskagile.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+/** =======================
+ *   Spring Security 설정
+    ======================= */
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  // 누구나 허용되는 경로를 정의
+  private static final String[] PUBLIC = new String[] {
+    "/error", "/login", "/logout", "/register", "/api/registrations"
+  };
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    // === Http 요청에 대한 security 설정 ===
+    http
+      .authorizeRequests()    //  http 요청에 기반한 접근 제한임을 알림
+        .antMatchers(PUBLIC).permitAll()  // 누구나 허용되는 경로 설정
+        .anyRequest().authenticated()     // PUBLIC외 다른 요청은 인증된 사용자만 접근 가능
+      .and()  // 메소드 호출 체인읠 http 객체로 복원
+        //.addFilterAt(authenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+        .formLogin()  // App가 form 기반 인증 설정
+          .loginPage("/login")  // 로그인 페이지 경로 설정
+      .and()
+        .logout()     // 로그아웃의 동작을 설정
+          .logoutUrl("/logout")   // 로그아웃 경로 설정(default)
+          .logoutSuccessUrl("/login?logged-out")  // 로그아웃 후 리다이렉트되는 경로
+          //.logoutSuccessHandler(LogoutSuccessHandler())
+      .and()
+        .csrf().disable()    //Cross-Site Request Forgery(크로스 사이트 요청 위조)기능을 disable
+      ;
+  }
+
+  @Override
+  public void configure(WebSecurity web) {
+    // web 요청에 대한 security 설정
+    // 아래의 경로에 대해서는 security 처리를 하지 않도록 설정하는 것으로 보임
+    web.ignoring().antMatchers("/static/**", "/js/**", "/css/**", "/images/**", "/favicon.ico");
+  }
+
+}
--- src/test/java/com/taskagile/web/apis/RegistrationApiControllerTests.java
+++ src/test/java/com/taskagile/web/apis/RegistrationApiControllerTests.java
@@ -3,6 +3,7 @@ package com.taskagile.web.apis;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
 
+import com.taskagile.config.SecurityConfiguration;
 import com.taskagile.domain.application.UserService;
 import com.taskagile.domain.model.user.EmailAddressExistsException;
 import com.taskagile.domain.model.user.UsernameExistsException;
@@ -15,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -22,7 +24,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(RegistrationApiController.class)
+@ContextConfiguration(classes = { SecurityConfiguration.class, RegistrationApiController.class })
+@WebMvcTest
 public class RegistrationApiControllerTests {
 
   @Autowired
